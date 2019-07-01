9bac042293d3ccf15cb5d2d348bc13adf2ae75db
Juan Javier <juan@pragmatic.agency>
2019-07-01 11:57:22 +0200

    [Change] Copy

--- src/pages/sobre-mi.js
+++ src/pages/sobre-mi.js
@@ -28,8 +28,8 @@ const SobreMiPage = () => {
                         <article id="post-564" className="post-564 page type-page status-publish post-password-required hentry">
                             <div className="entry-content">
                                 <p>Hello! Me llamo Javier Moreno y soy un apasiado diseñador web y desarrollador. ¿Las dos cosas? yeah!</p>
-                                <p>Estoy especialmente enfocado en desarrollo WordPress y diseño de temas personalizados. Actualmente colaboro como freelance para varias agencias de diseño web y social media, pero también acepto trabajos de pequeñas empresas.</p>
-                                <p>Vivo en Málaga, aunque me muevo por toda la provincia. También por Sevilla, Córdoba, Madrid y por Ibiza donde nací.</p>
+                                <p>Estoy especialmente enfocado en desarrollo WordPress y diseño de temas personalizados. Actualmente trabajo en Pragmatic Agency, UK.</p>
+                                <p>Vivo en Málaga, aunque trabajo en remoto y me encanta.</p>
                                 <h2>Sobre mí…</h2>
                                 <p>Llevo en el diseño web desde 1999, cuando empecé (de joven) usaba Hot Metal Pro, desde entonces hasta ahora he estado produciendo websites siguiendo la evolución en este área: desde los primeros editores WYSIWYG, las primeras webs realizadas con CSS, la era Flash, XHTML &amp; la validación, después HTML5-CSS3-jQuery y últimamente con PWAs y Gatsby.</p>
                                 <p>Anteriormente creaba las webs usando mi propio CMS, pero desde que descubrí WordPress en 2009 todo cambió en mi carrera. Dejé atrás las peleas con PHP, problemas de implementar nuevas funcionalidades, seguridad, etc. Todo desapareció con WordPress, que me ha dado más libertad para dedicar más tiempo a involucrarme en el diseño y trabajar con los clientes.</p>
